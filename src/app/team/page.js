423972021240542209436ae0864157854ef76758
inqueue0979 <inqueue0979@gmail.com>
2025-12-15 03:54:25 +0900

    Feat : Shellby Team 리팩토링 및 사진 변경

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,13 +1,11 @@
 import TeamHeroSection from "@/components/sections/team/TeamHeroSection";
 import TeamMembersSection from "@/components/sections/team/TeamMembersSection";
-import TeamStorySection from "@/components/sections/team/TeamStorySection";
 
 export default function Team() {
   return (
     <section>
         <TeamHeroSection></TeamHeroSection>
         <TeamMembersSection></TeamMembersSection>
-        <TeamStorySection></TeamStorySection>
     </section>
   );
 }
